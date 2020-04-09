00c9983c7f123afc598b82d9f2cb8da185072b1a
Khaled Md Tuhidul Hossain <mth.tuhin@gmail.com>
2020-04-10 00:24:24 +0600

    Test Case Input Added

--- main.go
+++ main.go
@@ -1,29 +1,36 @@
 package main
 
 import (
-	"fmt"
 	"bufio"
+	"fmt"
 	"os"
 	"strconv"
 )
 
-var Totn int
+var totaltestcases int
 var tot []int
 
 func main() {
-	var outerloop int
 
+	totaltestcases := testcaseinput()
+
+	fmt.Println("Total Test Cases : " + strconv.Itoa(totaltestcases))
+}
+
+func testcaseinput() int {
+
+	var outerloop int
+	fmt.Print("Total Number Of Test Cases : ")
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
-		
+
 	outerloop, err := strconv.Atoi(scanner.Text())
-    if err != nil{
-    	fmt.Println("outerloop error")
-    }
-	
+	if err != nil {
+		fmt.Println("Not a Valid Number. Please try again")
+		testcaseinput()
+	}
 	if err := scanner.Err(); err != nil {
 		fmt.Fprintln(os.Stderr, "reading standard input:", err)
 	}
-    Totn = outerloop
-	fmt.Println(Totn)
+	return outerloop
 }

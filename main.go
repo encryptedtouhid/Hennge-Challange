bf8a4ccfaf18bf08e6157f8419e64971b56ea09a
Khaled Md Tuhidul Hossain <mth.tuhin@gmail.com>
2020-04-09 23:52:11 +0600

    checking Basic Operation

--- main.go
+++ main.go
@@ -1,9 +1,29 @@
-package Hennge_Challange
+package main
 
-import "fmt"
+import (
+	"fmt"
+	"bufio"
+	"os"
+	"strconv"
+)
+
+var Totn int
+var tot []int
 
 func main() {
+	var outerloop int
 
-	fmt.Println("hi")
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+		
+	outerloop, err := strconv.Atoi(scanner.Text())
+    if err != nil{
+    	fmt.Println("outerloop error")
+    }
+	
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	}
+    Totn = outerloop
+	fmt.Println(Totn)
 }
-

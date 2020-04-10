6e3aabfa78868ec1e3cab3cbedde5732d3dd1978
Khaled Md Tuhidul Hossain <mth.tuhin@gmail.com>
2020-04-10 13:20:55 +0600

    Added Number of inputs

--- main.go
+++ main.go
@@ -8,14 +8,18 @@ import (
 )
 
 var totaltcs int
+var numberofint int
 var tot []int
 
 func main() {
 	totaltcs := testcaseinput()
+	numberofint := inputount()
 	fmt.Println("Total Test Cases : " + strconv.Itoa(totaltcs))
+	fmt.Println("Total Number of Inputs : " + strconv.Itoa(numberofint))
 }
 
 func testcaseinput() int {
+
 	var totaltestcases int
 
 	fmt.Print("Total Number Of Test Cases : ")
@@ -31,4 +35,24 @@ func testcaseinput() int {
 		fmt.Fprintln(os.Stderr, "Reading standard input:", err)
 	}
 	return totaltestcases
+}
+
+func inputount() int {
+
+	var inputcount int
+
+	fmt.Print("Total Number Of Input : ")
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+
+	inputcount, err := strconv.Atoi(scanner.Text())
+
+	if err != nil {
+		fmt.Println("Not a Valid Number. Please try again")
+		return inputount()
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "Reading standard input:", err)
+	}
+	return inputcount
 }

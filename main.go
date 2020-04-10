1d1d7529d0f5bf7ad47f40e3a404e33a2f7ca4b3
Khaled Md Tuhidul Hossain <mth.tuhin@gmail.com>
2020-04-10 18:18:35 +0600

    Update main.go

--- main.go
+++ main.go
@@ -18,12 +18,11 @@ var resultonetemp int
 var result int
 
 func main() {
-	// totaltcs := testcaseinput()
-	// numberofint := inputount()
-	// fmt.Println("Total Test Cases : " + strconv.Itoa(totaltcs))
-	// fmt.Println("Total Number of Inputs : " + strconv.Itoa(numberofint))
-
-	// fmt.Println("Inputs : " + getinput())
+	totaltcs := testcaseinput()
+	numberofint := inputount()
+	fmt.Println("Total Test Cases : " + strconv.Itoa(totaltcs))
+	fmt.Println("Total Number of Inputs : " + strconv.Itoa(numberofint))
+	fmt.Println("Inputs : " + getinput())
 	testArray := strings.Fields(getinput())
 	stringtoarray(testArray, index)
 	stepone(inputs, 0)
